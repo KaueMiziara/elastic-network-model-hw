baf91407ebf105c250cb3113ae98d02c2ee7339c
Kaue Miziara <kauemiziara@gmail.com>
2026-08-19 20:39:30 -0300

    step 5.0: clean step 4 script

---
+++ scripts/step05.py
@@ -0,0 +1,56 @@
+import numpy as np
+
+# Step 1
+
+coordinates = []
+
+with open("data/1AKI.pdb", "r") as file:
+    for line in file:
+        if line.startswith("ATOM"):
+            atom_name = line[12:16].strip()
+
+            if atom_name == "CA":
+                x = float(line[30:38])
+                y = float(line[38:46])
+                z = float(line[46:54])
+
+                coordinates.append([x, y, z])
+
+ca_matrix = np.array(coordinates)
+
+# Step 2
+
+diff = ca_matrix[:, np.newaxis, :] - ca_matrix[np.newaxis, :, :]
+
+sq_diff = diff**2
+sum_sq_diff = np.sum(sq_diff, axis=2)
+distance_matrix = np.sqrt(sum_sq_diff)
+
+# Step 3
+
+cutoff = 8.0
+
+contact_map = (distance_matrix <= cutoff).astype(int)
+
+np.fill_diagonal(contact_map, 0)
+
+kirchhoff = -1 * contact_map
+
+degrees = np.sum(contact_map, axis=1)
+
+np.fill_diagonal(kirchhoff, degrees)
+
+# Step 4
+
+eigenvalues, eigenvectors = np.linalg.eigh(kirchhoff)
+
+idx = eigenvalues.argsort()
+eigenvalues = eigenvalues[idx]
+eigenvectors = eigenvectors[:, idx]
+
+non_zero_evals = eigenvalues[1:]
+non_zero_evecs = eigenvectors[:, 1:]
+
+fluctuations = np.sum((non_zero_evecs**2) / non_zero_evals, axis=1)
+
+print(f"Calculated fluctuations for {len(fluctuations)} atoms.")
